f2ececfcb43d9b41837b4379cc438f3112da3655
ASHISH KUMAt <akumaraman966@gmail.com>
2022-09-06 14:28:55 +0530

    update
    update

--- Boostrap/boostrap/src/index.js
+++ Boostrap/boostrap/src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter,Route, Routes} from 'react-router-dom';
-import Feture from './Component/Feture';
-import Pricing from './Component/Pricing';
-
-
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" component={<App/> } />
-    <Route path="expenses" component={Feture}/>
-    <Route path="invoices" element={<Pricing />} />
-  </Routes>
-</BrowserRouter>
-   
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
